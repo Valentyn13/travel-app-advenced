5284b1e5bae7cd6d742563b48ce99f7b299278a5
Valentyn13 <valentyn13git@gmail.com>
2024-01-21 18:40:19 +0200

    add state to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,20 @@ import SignUpPage from "./pages/sign-up/sign-up"
 import SignInPage from "./pages/sign-in/sign-in"
 import TripDeteilsPage from "./pages/trip-details/trip-details"
 import BookingsPage from "./pages/bookings/bookings"
+import { useState } from "react"
+import { ITripList } from "./types/trip.types"
+import { BOOKINGS, TRIPS } from "./data/data"
+import { IBookingList } from "./types/booking.types"
 
 const App =() => {
+  const [trips, setTrips] = useState<ITripList>(TRIPS)
+  const [bookings, setBookings] = useState<IBookingList>(BOOKINGS)
+
   return (
     <>
       <Routes>
           <Route path={ROUTES.MAIN} element={<Layout/>}>
-              <Route index element={<MainPage/>}/>
+              <Route index element={<MainPage onTripsChange={setTrips} trips={trips}/>}/>
               <Route path={ROUTES.SIGN_UP} element={<SignUpPage/>}/>
               <Route path={ROUTES.SIGN_IN} element={<SignInPage/>}/>
               <Route path={ROUTES.TRIP_DETAILS} element={<TripDeteilsPage/>}/>
