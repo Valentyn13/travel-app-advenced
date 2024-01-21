f16ad211230daa956294836102085d02b868983e
Valentyn13 <valentyn13git@gmail.com>
2024-01-21 18:41:54 +0200

    render TripList in Main component

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,6 +1,13 @@
-import { Link } from "react-router-dom";
+import { Dispatch, FC } from "react";
+import TripList from "../../components/trip-list/trip-list";
+import { ITripList } from "../../types/trip.types";
 
-const MainPage = () => {
+type Props = {
+  trips:ITripList,
+  onTripsChange:Dispatch<React.SetStateAction<ITripList>>
+}
+
+const MainPage:FC<Props> = ({trips}) => {
     return(
         <main>
         <h1 className="visually-hidden">Travel App</h1>
@@ -36,49 +43,7 @@ const MainPage = () => {
             </label>
           </form>
         </section>
-        <section className="trips">
-          <h2 className="visually-hidden">Trips List</h2>
-          <ul className="trip-list">
-            <li data-test-id="trip-card" className="trip-card">
-              <img
-                data-test-id="trip-card-image"
-                src="/src/assets/images/iceland.jpg"
-                alt="trip photo"
-              />
-              <div className="trip-card__content">
-                <div className="trip-info">
-                  <h3 data-test-id="trip-card-title" className="trip-info__title">
-                    Iceland
-                  </h3>
-                  <div className="trip-info__content">
-                    <span
-                      data-test-id="trip-card-duration"
-                      className="trip-info__duration"
-                    >
-                      <strong>15</strong> days
-                    </span>
-                    <span data-test-id="trip-card-level" className="trip-info__level">
-                      easy
-                    </span>
-                  </div>
-                </div>
-                <div className="trip-price">
-                  <span>Price</span>
-                  <strong
-                    data-test-id="trip-card-price-value"
-                    className="trip-price__value"
-                  >
-                    7000 $
-                  </strong>
-                </div>
-              </div>
-              {/* // TODO: remove hard code */}
-              <Link data-test-id="trip-card-link" to="/trip/123" className="button">
-                Discover a trip
-              </Link>
-            </li>
-          </ul>
-        </section>
+          <TripList trips={trips}/>
       </main>
     )
 }
