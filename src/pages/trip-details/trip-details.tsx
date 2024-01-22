92f411df19fe5144a2ee185ed7ff232a229ceb87
Valentyn13 <valentyn13git@gmail.com>
2024-01-22 16:30:53 +0200

    add details logic

--- src/pages/trip-details/trip-details.tsx
+++ src/pages/trip-details/trip-details.tsx
@@ -1,17 +1,37 @@
 import { useLocation } from "react-router";
  import { ITrip } from "../../types/trip.types";
+import { Dispatch, FC, SetStateAction, useState } from "react";
+import TripPopup from "./trip-popup";
+import { IUser } from "../../types/user.types";
+import { IBookingList } from "../../types/booking.types";
 
 type ITripDetails ={
   trip: ITrip
 }
 
-const TripDeteilsPage = () => {
+type Props = {
+  user:null|IUser;
+  bookings:IBookingList
+  setBookings: Dispatch<SetStateAction<IBookingList>>
+}
+
+const TripDeteilsPage:FC<Props> = ({user, bookings, setBookings}) => {
 
   const location = useLocation()
  //  const { tripId } = useParams()
 
   const {trip} = location.state as ITripDetails
   const {title, image, description, duration, price, level} = trip
+
+  const [isBookModalOpen, setIsBookModalOpen] = useState(false)
+
+  const handleOpenBookModal = () => {
+    setIsBookModalOpen(true)
+  }
+  const handleCloseBookModal = () => {
+    setIsBookModalOpen(false)
+  }
+
     return(
         <main className="trip-page">
         <h1 className="visually-hidden">Travel App</h1>
@@ -55,6 +75,7 @@ const TripDeteilsPage = () => {
               </strong>
             </div>
             <button
+            onClick={handleOpenBookModal}
               data-test-id="trip-details-button"
               className="trip__button button"
             >
@@ -62,6 +83,14 @@ const TripDeteilsPage = () => {
             </button>
           </div>
         </div>
+        {isBookModalOpen && (
+        <TripPopup 
+        trip={trip} 
+        onClose={handleCloseBookModal}
+        user={user}
+        bookings={bookings}
+        setBookings={setBookings}
+        />)}
       </main>
     )
 }
