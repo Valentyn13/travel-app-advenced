2ca11fdf26c6858c7fcc88ae1b5647ffae6012a5
Valentyn13 <valentyn13git@gmail.com>
2024-01-22 16:29:57 +0200

    add sign in logic

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,14 +1,58 @@
 import { Link, useNavigate  } from "react-router-dom";
 import { ROUTES } from "../../types/routes.types";
-import { FormEvent } from "react";
+import { ChangeEvent, Dispatch, FC, FormEvent, SetStateAction, useState } from "react";
+import { IUser } from "../../types/user.types";
+import { isValidEmail, isPasswordLenghtValid } from "../../helpers/email.helpers";
+type Props ={
+  setUser:Dispatch<SetStateAction<IUser | null>>
+}
 
-const SignInPage = () => {
+const SignInPage:FC<Props> = ({setUser}) => {
   const navigate = useNavigate()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [validEmail, setValidEmail] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false)
+
+  const handleEmailInput = (e:ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value
+    setEmail(email)
+  }
+
+  const handlePasswordInput = (e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setPassword(value)
+  }
+
+
+  const handleValidateEmail= () => {
+    const valid = isValidEmail(email)
+    setValidEmail(valid)
+  }
+
+  const handlePasswordValidate = () => {
+    const valid = isPasswordLenghtValid(password)
+    setPasswordValid(valid)
+  }
+
 
   const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    setUser({
+      fullName: 'Test',
+      email,
+      password,
+      id: self.crypto.randomUUID()
+    })
+
+    navigate(ROUTES.MAIN)
     navigate(ROUTES.MAIN)
   }
+
+
     return(
         <main className="sign-in-page">
         <h1 className="visually-hidden">Travel App</h1>
@@ -16,19 +60,23 @@ const SignInPage = () => {
           <h2 className="sign-in-form__title">Sign In</h2>
           <label className="input">
             <span className="input__heading">Email</span>
-            <input data-test-id="auth-email" name="email" type="email" required />
+            <input data-test-id="auth-email" name="email" type="email" onChange={handleEmailInput} onBlur={handleValidateEmail} required />
+            {!validEmail && (<div>Email is not valid</div>)}
           </label>
           <label className="input">
             <span className="input__heading">Password</span>
             <input
+              onChange={handlePasswordInput}
+              onBlur={handlePasswordValidate}
               data-test-id="auth-password"
               name="password"
               type="password"
               autoComplete="new-password"
               required
             />
+            {!passwordValid && (<div>Password must have from 3 to 20 characters</div>)}
           </label>
-          <button data-test-id="auth-submit" className="button" type="submit">
+          <button disabled={!validEmail || !passwordValid} data-test-id="auth-submit" className="button" type="submit">
             Sign In
           </button>
         </form>
