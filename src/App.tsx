621e5946a96f8187a77ed57c2a0c942ef1e7210e
Valentyn13 <valentyn13git@gmail.com>
2024-01-22 16:31:17 +0200

    create state for user

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,21 @@ import { useState } from "react"
 import { ITripList } from "./types/trip.types"
 import { BOOKINGS, TRIPS } from "./data/data"
 import { IBookingList } from "./types/booking.types"
+import { IUser } from "./types/user.types"
 
 const App =() => {
   const [trips, setTrips] = useState<ITripList>(TRIPS)
   const [bookings, setBookings] = useState<IBookingList>(BOOKINGS)
-
+  const [user, setUser] = useState<null|IUser>(null)
+  console.log(user)
   return (
     <>
       <Routes>
           <Route path={ROUTES.MAIN} element={<Layout/>}>
               <Route index element={<MainPage onTripsChange={setTrips} trips={trips}/>}/>
-              <Route path={ROUTES.SIGN_UP} element={<SignUpPage/>}/>
-              <Route path={ROUTES.SIGN_IN} element={<SignInPage/>}/>
-              <Route path={ROUTES.TRIP_DETAILS} element={<TripDeteilsPage/>}/>
+              <Route path={ROUTES.SIGN_UP} element={<SignUpPage setUser={setUser}/>}/>
+              <Route path={ROUTES.SIGN_IN} element={<SignInPage setUser={setUser}/>}/>
+              <Route path={ROUTES.TRIP_DETAILS} element={<TripDeteilsPage bookings={bookings} setBookings={setBookings} user={user}/>}/>
               <Route path={ROUTES.BOOKINGS} element={<BookingsPage setBookings={setBookings} bookings={bookings}/>}/>
               <Route path="*" element={<Navigate to={ROUTES.MAIN}/>}/>
           </Route>
