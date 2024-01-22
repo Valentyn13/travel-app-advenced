7d9df399c920f9b51fbee3a1aef634c203a1a1e9
Valentyn13 <valentyn13git@gmail.com>
2024-01-22 16:30:36 +0200

    add trip modal logic

--- src/pages/trip-details/trip-popup.tsx
+++ src/pages/trip-details/trip-popup.tsx
@@ -1,37 +1,124 @@
-const TripPopup = () => {
+import {  ChangeEvent, Dispatch, FC, FormEvent, SetStateAction, useState } from "react";
+import { ITrip } from "../../types/trip.types";
+import { formatDate, isDateBeforeCurrent } from "../../helpers/date.helpers";
+import { IBooking, IBookingList } from "../../types/booking.types";
+import { IUser } from "../../types/user.types";
+
+type Props = {
+  user:null|IUser;
+  bookings:IBookingList
+  setBookings: Dispatch<SetStateAction<IBookingList>>
+  trip: ITrip
+  onClose:() => void
+}
+
+const TripPopup:FC<Props> = ({user,bookings,setBookings,trip, onClose}) => {
+  const {title, duration, level} = trip
+
+  const[guests, setGuests] = useState<number>(1)
+  const [date, setDate] = useState(formatDate(new Date().toISOString()))
+  const [isNumberOfGuestsValid, setIsNumberOfGuestsValid] = useState(true)
+
+  const validateNumberOfGuests = (guests:number) => {
+    if (guests < 1) {
+      setIsNumberOfGuestsValid(false)
+      return
+    }
+    if (guests > 10){
+      setIsNumberOfGuestsValid(false)
+      return
+    }
+    setIsNumberOfGuestsValid(true)
+    
+  }
+
+  const validateDate = (newDate:string) => {
+    const isBefore = isDateBeforeCurrent(newDate)
+    if(isBefore) {
+
+      setDate(formatDate(new Date().toISOString()))
+      return
+    }
+    setDate(newDate)
+  }
+
+ const handleSetGuests = (e: ChangeEvent<HTMLInputElement>) => {
+  const value = +e.target.value
+  validateNumberOfGuests(value)
+  setGuests(value)
+ }
+
+ const handleSetDate = (e:ChangeEvent<HTMLInputElement>) => {
+  const  date = e.target.value
+  validateDate(date)
+ }
+  
+
+ const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  if (user){
+    const newBooking:IBooking = {
+      id: self.crypto.randomUUID(),
+      userId: user.id,
+      tripId:trip.id,
+      guests,
+      date,
+      trip:{
+        title:trip.title,
+        duration:trip.duration,
+        price:trip.price
+      },
+      totalPrice: guests * trip.price,
+      createdAt: new Date().toISOString()
+    }
+
+    setBookings([...bookings, newBooking])
+    onClose()
+    console.log('Submit')
+  } else {
+    alert('Please authorize')
+  }
+ }
+
+
+
     return(
-        <div hidden>
+        <div>
         <div className="modal">
           <div data-test-id="book-trip-popup" className="book-trip-popup">
             <button
+            onClick={onClose}
               data-test-id="book-trip-popup-close"
               className="book-trip-popup__close"
             >
               ×
             </button>
-            <form className="book-trip-popup__form" autoComplete="off">
+            <form onSubmit={handleFormSubmit} className="book-trip-popup__form" autoComplete="off">
               <div className="trip-info">
                 <h3 data-test-id="book-trip-popup-title" className="trip-info__title">
-                  Iceland
+                  {title}
                 </h3>
                 <div className="trip-info__content">
+                  <img src="/src/assets/images/calendar.svg" alt="calendar" />
                   <span
                     data-test-id="book-trip-popup-duration"
                     className="trip-info__duration"
                   >
-                    <strong>15</strong> days
+                    <strong>{duration}</strong> days
                   </span>
                   <span
                     data-test-id="book-trip-popup-level"
                     className="trip-info__level"
                   >
-                    easy
+                    {level}
                   </span>
                 </div>
               </div>
               <label className="input">
                 <span className="input__heading">Date</span>
                 <input
+                  value={date}
+                  onChange={handleSetDate}
                   data-test-id="book-trip-popup-date"
                   name="date"
                   type="date"
@@ -41,14 +128,14 @@ const TripPopup = () => {
               <label className="input">
                 <span className="input__heading">Number of guests</span>
                 <input
+                  onChange={handleSetGuests}
                   data-test-id="book-trip-popup-guests"
                   name="guests"
                   type="number"
-                  min="1"
-                  max="10"
-                  value="1"
+                  value={guests}
                   required
                 />
+                {!isNumberOfGuestsValid && (<div>Only from 0 to 10 guests</div>)}
               </label>
               <span className="book-trip-popup__total">
                 Total:
@@ -56,13 +143,14 @@ const TripPopup = () => {
                   data-test-id="book-trip-popup-total-value"
                   className="book-trip-popup__total-value"
                 >
-                  4000$
+                  {guests * trip.price}$
                 </output>
               </span>
               <button
                 data-test-id="book-trip-popup-submit"
                 className="button"
                 type="submit"
+                disabled={!isNumberOfGuestsValid}
               >
                 Book a trip
               </button>
