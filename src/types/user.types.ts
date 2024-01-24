a276fdab20f1681e0587198f96829f7944189a78
Valentyn13 <valentyn13git@gmail.com>
2024-01-24 20:28:49 +0200

    advanced: add user types

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,7 +1,37 @@
 export interface IUser {
   id: string;
   email: string;
-  password: string;
   fullName: string;
   createdAt?:string
+}
+
+export interface SignUpUserDto {
+  fullName: string,
+  email:string,
+  password: string
+}
+
+export interface SignInUserDto {
+  email: string,
+  password: string
+}
+
+
+export interface UserResponseDto {
+  token:string,
+  user: {
+    id: string;
+    email: string;
+    fullName: string;
+    createdAt:string
+  }
+}
+
+
+
+export interface IAuthorizedUserResponse {
+  id: string
+  fullName: string
+  email: string
+  createdAt: string
 }
