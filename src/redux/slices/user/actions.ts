3a556984c9259290a1953ccb13d033c39bbfd667
Valentyn13 <valentyn13git@gmail.com>
2024-01-24 20:36:08 +0200

    advanced: add user redux logic

---
+++ src/redux/slices/user/actions.ts
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { IAuthorizedUserResponse, SignInUserDto, SignUpUserDto, UserResponseDto } from "../../../types/user.types";
+import userApi from "../../../services/user.service";
+
+const signUpUser = createAsyncThunk<UserResponseDto,SignUpUserDto>('sign-up', async (data) => {
+    const user = await userApi.signUp(data)
+    return user
+})
+
+const signInUser = createAsyncThunk<UserResponseDto,SignInUserDto>('sign-in', async(data) => {
+    const user = await userApi.signIn(data)
+    return user
+})
+
+const getAuthenticatedUser = createAsyncThunk<IAuthorizedUserResponse, void>('authed', async() => {
+    const user = await userApi.getAuthenticatedUser()
+    return user
+})
+
+
+export {signInUser, signUpUser, getAuthenticatedUser}
--- src/redux/slices/user/user.slice.ts
+++ src/redux/slices/user/user.slice.ts
@@ -1,25 +1,72 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
 import { IUser } from "../../../types/user.types"
+import { getAuthenticatedUser, signInUser, signUpUser } from "./actions"
 
 type State ={
     user: IUser | null
+    loading:boolean,
+    error:unknown | null   
 }
 
 const initialState: State ={
-    user: null
+    user: null,
+    loading:false,
+    error: null
 }
 
-
-const {reducer, actions, name} = createSlice({
+const {reducer, name} = createSlice({
     initialState,
     name:'user',
-    reducers:{
-        testAction: (state, action:PayloadAction<string>) => {
-            console.log(action.payload)
-        },
-    },
-    extraReducers(){}
-})
+    reducers:{},
+    extraReducers: (builder)=>{
+        builder.addCase(signInUser.fulfilled,(state, action) => {
+            const {token, user} = action.payload
 
-export const {testAction} = actions
+            localStorage.setItem('token', token)
+
+            state.user = user
+            state.loading = false
+            state.error = null
+
+        });
+        builder.addCase(signInUser.pending,(state) => {
+            state.loading = true
+            state.error = null
+        });
+        builder.addCase(signInUser.rejected,(state, action) => {
+            state.loading = false
+            state.error = action.error
+            console.error(action.error)
+        });
+
+        builder.addCase(signUpUser.fulfilled,(state, action) => {
+            const {token, user} = action.payload
+            localStorage.setItem('token', token)
+            state.user = user
+        });
+        builder.addCase(signUpUser.pending,(state) => {
+            state.loading = true
+            state.error = null
+        });
+        builder.addCase(signUpUser.rejected,(state, action) => {
+            state.loading = false
+            state.error = action.error
+            console.error(action.error)
+        });
+        builder.addCase(getAuthenticatedUser.fulfilled, (state, action) => {
+            state.user = action.payload
+        })
+        builder.addCase(getAuthenticatedUser.pending,(state) => {
+            state.loading = true
+            state.error = null
+        });
+        builder.addCase(getAuthenticatedUser.rejected,(state,action) => {
+            localStorage.removeItem('token')
+            state.error = action.error
+            state.user = null
+            state.loading = false
+            console.error(action.error)
+        })
+    }
+})
 export {reducer, name}
