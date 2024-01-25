136718b078ae09ae36e3693cf869ea60965e5434
Valentyn13 <valentyn13git@gmail.com>
2024-01-25 09:42:30 +0200

    advanced: add getAllTrips redux logic

---
+++ src/redux/slices/trips/actions.ts
@@ -0,0 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ITrip, ITripList } from "../../../types/trip.types";
+import tripApi from "../../../services/trips.service";
+
+const getAllTrips = createAsyncThunk<ITripList,void>('get-all',async () =>{
+ const trips = await tripApi.getAllTrips()
+ return trips
+})
+
+const getTripDetails = createAsyncThunk<ITrip, string>('get-details',(tripId) => {
+    const trip = tripApi.getTripDetails(tripId)
+    return trip
+})
+
+export {getAllTrips, getTripDetails}
--- src/redux/slices/trips/trips.slice.ts
+++ src/redux/slices/trips/trips.slice.ts
@@ -1,19 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { ITripList } from "../../../types/trip.types"
+import { getAllTrips } from "./actions"
 
 type State ={
-    trips: ITripList
+    trips: ITripList,
+    loading:boolean,
+    error:unknown|null
 }
 
 const initialState: State ={
-    trips:[]
+    trips:[],
+    loading:false,
+    error:null
 }
 
 const {reducer, actions, name} = createSlice({
     initialState,
     name:'trips',
     reducers:{},
-    extraReducers(){}
+    extraReducers:(builder) =>{
+        builder.addCase(getAllTrips.pending,(state) => {
+            state.loading = true
+            state.error = null
+        });
+        builder.addCase(getAllTrips.fulfilled,(state, action) => {
+            state.trips = [...action.payload]
+            state.loading = false
+            state.error = null
+        });
+        builder.addCase(getAllTrips.rejected,(state,action) => {
+            state.error = action.error
+            state.loading = false
+        })
+    }
 })
 
 
