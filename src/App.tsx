9893134b0b77050a3b5b90a51651236b01a07600
Valentyn13 <valentyn13git@gmail.com>
2024-01-25 16:34:07 +0200

    advanced: remove some props in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import {  useState } from "react";
 import { Navigate, Route, Routes  } from "react-router-dom";
 
 import Layout from "./pages/layout/layout";
@@ -8,19 +7,13 @@ import SignInPage from "./pages/sign-in/sign-in";
 import TripDeteilsPage from "./pages/trip-details/trip-details";
 import BookingsPage from "./pages/bookings/bookings";
 
-import { BOOKINGS } from "./data/data";
-import { IBookingList } from "./types/booking.types";
 import { ROUTES } from "./types/routes.types";
 import PrivateRoute from "./components/private-route/private-rote";
 import { useAppSelector } from "./redux/hooks";
 
 
 const App =() => {
-
   const user = useAppSelector(state => state.user.user)
-  
-  const [bookings, setBookings] = useState<IBookingList>(BOOKINGS)
-
   return (
     <>
       <Routes>
@@ -32,12 +25,12 @@ const App =() => {
               <Route path={ROUTES.SIGN_IN} element={<SignInPage/>}/>
               <Route path={ROUTES.TRIP_DETAILS} element={
                 <PrivateRoute user={user} redirectPath={ROUTES.SIGN_IN}>
-                  <TripDeteilsPage bookings={bookings} setBookings={setBookings}/>
+                  <TripDeteilsPage />
                 </PrivateRoute>
                 }/>
               <Route path={ROUTES.BOOKINGS} element={
                 <PrivateRoute user={user} redirectPath={ROUTES.SIGN_IN}>
-                  <BookingsPage setBookings={setBookings} bookings={bookings}/>
+                  <BookingsPage/>
                 </PrivateRoute>
               }/>
               <Route path="*" element={
