0e0b43aeb12597c84129abb65e05b2776a092e08
Valentyn13 <valentyn13git@gmail.com>
2024-01-25 09:41:52 +0200

    advanced: create trip service

---
+++ src/services/trips.service.ts
@@ -0,0 +1,33 @@
+import { ITrip, ITripList } from "../types/trip.types"
+import httpApi from "./http.service"
+
+const BASE_URL = 'https://travel-app-api.up.railway.app/api/v1'
+
+class TripApi {
+    #getAll = '/trips'
+    #getDetails= '/trips/'
+
+
+    async getAllTrips():Promise<ITripList> {
+        return httpApi.load(`${BASE_URL}${this.#getAll}`,{
+            method: 'GET',
+            payload: null,
+            hasAuth: true,
+            contentType:'application/json'
+        })
+    }
+
+
+    async getTripDetails(tripId:string):Promise<ITrip> {
+        return httpApi.load(`${BASE_URL}${this.#getDetails}${tripId}`,{
+            method:'GET',
+            payload: null,
+            hasAuth: true,
+            contentType: 'application/json'
+        })
+    }
+}
+
+
+const tripApi = new TripApi()
+export default tripApi;
